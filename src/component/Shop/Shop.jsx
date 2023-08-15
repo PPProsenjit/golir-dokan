70276e93d79777d535f088eafdaf87c445467b65
PPProsenjite <prosenjitpappu03455@gmial.com>
2023-08-15 11:11:58 +0600

    products cart

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,10 +1,27 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+import Product from '../Product/Product';
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res =>res.json())
+        .then(data => setProducts(data))
+    },[])
     return (
-        <div>
-            
+        <div className='shop-container'>
+            <div className='items-container'>
+                {
+                    products.map(product => <Product 
+                        key = {product.id}
+                        product = {product}
+                    ></Product>)
+                }
+            </div>
+            <div className='orders-container'>
+                <h1>Order</h1>
+            </div>
         </div>
     );
 };
